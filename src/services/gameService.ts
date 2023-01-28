73a5817e0035669d608ac56018285ef6e4daa2e9
Chewdawg <wesleyshynes@gmail.com>
2023-01-28 18:54:25 -0500

    compiler

--- src/services/gameService.ts
+++ src/services/gameService.ts
@@ -521,7 +521,7 @@ class GameService {
         const {
             effectId,
             effectArgs,
-            effectType
+            // effectType
         } = card;
 
         const effectDetails = noveltyEffectService.getEffectDetails(effectId);
